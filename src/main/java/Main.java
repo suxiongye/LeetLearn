b59b6ca4e7b924e101d7404e99c7ed79ee414a80
suxiongye <suxiongye@baidu.com>
2020-10-14 15:51:03 +0800

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * TODO
+ *
+ * @author suxiongye
+ * @date 2020/10/14 3:46 下午
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
