b8a5599ed63f82591c984fe7c44fc812261b6c54
suxiongye <suxiongye@baidu.com>
2020-10-15 20:56:38 +0800

    Q-3 题目

---
+++ src/main/java/MedianTwoSortedArrays.java
@@ -0,0 +1,72 @@
+/**
+ *  https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+ *
+ * @author suxiongye
+ * @date 2020/10/15 7:48 下午
+ * @description
+ *     给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数。
+ *
+ *  进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+ *
+ *
+ *
+ *  示例 1：
+ *
+ *  输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ *
+ *
+ *  示例 2：
+ *
+ *  输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ *
+ *
+ *  示例 3：
+ *
+ *  输入：nums1 = [0,0], nums2 = [0,0]
+ * 输出：0.00000
+ *
+ *
+ *  示例 4：
+ *
+ * 输入：nums1 = [], nums2 = [1]
+ * 输出：1.00000
+ *
+ *
+ *  示例 5：
+ *
+ *  输入：nums1 = [2], nums2 = []
+ * 输出：2.00000
+ *
+ *
+ *
+ *
+ *  提示：
+ *
+ *
+ *  nums1.length == m
+ *  nums2.length == n
+ *  0 <= m <= 1000
+ *  0 <= n <= 1000
+ *  1 <= m + n <= 2000
+ *  -106 <= nums1[i], nums2[i] <= 106
+ *
+ *  Related Topics 数组 二分查找 分治算法
+ */
+public class MedianTwoSortedArrays {
+    /**
+     * 参考：https://blog.csdn.net/bjweimengshu/article/details/97717144
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0.0;
+    }
+
+    // 解法1 ：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xun-zhao-liang-ge-you-xu-shu-zu-de-zhong-wei-s-114/
+
+}
